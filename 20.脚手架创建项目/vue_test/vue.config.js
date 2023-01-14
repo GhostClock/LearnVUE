b09c699ed5dcc9400a0ddf9eabf79ac6deac77ff
GhostClock <zhangyuanlaifen@gmail.com>
2023-01-15 00:28:32 +0800

    在vue.config.js中修改默认配置

--- 20.脚手架创建项目/vue_test/vue.config.js
+++ 20.脚手架创建项目/vue_test/vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      // 自定义入口文件
+      entry: 'src/main.js',
+
+    },
+  },
   lintOnSave: false, // 忽略语法规则错误 
 })
