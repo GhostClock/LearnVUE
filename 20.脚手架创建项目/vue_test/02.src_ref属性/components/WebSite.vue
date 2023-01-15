5f8a2c46f921e552c5976183c17d4519c4682a5a
GhostClock <zhangyuanlaifen@gmail.com>
2023-01-15 12:36:26 +0800

    02.ref属性

---
+++ 20.脚手架创建项目/vue_test/02.src_ref属性/components/WebSite.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="website">
+    <h1>网站名称：{{ name }}</h1>
+    <h1>网站地址：{{ address }}</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "WebSite",
+        data() {
+            return {
+                name: "百度",
+                address: "北京",
+            }
+        },
+    }
+</script>
+
+<style>
+    .website{
+        background-color: gray;
+    }
+</style>
