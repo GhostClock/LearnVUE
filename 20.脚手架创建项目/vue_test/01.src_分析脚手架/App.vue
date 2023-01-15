d238ad85ca0aaa358edf288af0ba5e1dd8123ff8
GhostClock <zhangyuanlaifen@gmail.com>
2023-01-15 12:35:47 +0800

    01.分析脚手架

---
+++ 20.脚手架创建项目/vue_test/01.src_分析脚手架/App.vue
@@ -0,0 +1,23 @@
+
+<!-- 安装了Vutur插件，就会有提示-->
+
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo"/>
+    <WebSite></WebSite>
+    <UserInfo></UserInfo>
+  </div>
+</template>
+
+<script>
+    // 引入组件
+    import WebSite from './components/WebSite.vue'
+    import UserInfo from './components/UserInfo.vue'
+
+    export default {
+        name: "App",
+        components:{
+          WebSite, UserInfo
+        }
+    }
+</script>
