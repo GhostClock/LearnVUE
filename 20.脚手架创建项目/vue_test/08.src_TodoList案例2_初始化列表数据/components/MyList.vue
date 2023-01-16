8505b26ec61884258a22f79682592ea6630be67d
GhostClock <zhangyuanlaifen@gmail.com>
2023-01-16 23:06:01 +0800

    TodoList案例2：初始化列表数据

---
+++ 20.脚手架创建项目/vue_test/08.src_TodoList案例2_初始化列表数据/components/MyList.vue
@@ -0,0 +1,44 @@
+<!-- 中间内容组件 -->
+<template>
+    <ul class="todo-main">
+        <MyItem v-for ="todoObj in todos" 
+        :key="todoObj.id" :todo="todoObj"/>
+    </ul>
+</template>
+
+<script>
+    import MyItem from "./MyItem";
+
+    export default {
+        name: "MyList",
+        components: { MyItem },
+        data() {
+            return {
+                todos:[
+                    {id: '001', title: "吃饭", done: true},
+                    {id: '002', title: "抽烟", done: false},
+                    {id: '003', title: "喝酒", done: false},
+                    {id: '004', title: "洗脚", done: true},
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    /* List */
+    .todo-main {
+        margin-left: 0px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding: 0px;
+    }
+    .todo-empty {
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding-left: 5px;
+        margin-top:10px;
+    }
+</style>
