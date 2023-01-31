497009b9f57b3c16b7d3a6d6ba852e23a2e315ec
GhostClock <zhangyuanlaifen@gmail.com>
2023-01-31 21:41:31 +0800

    TodoList案例3：添加数据

---
+++ 20.脚手架创建项目/vue_test/09.src_TodoList案例3_添加数据/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="todu-container">
+    <div class="todo-wrap">
+      <MyHeader :addTodo="addTodo"/>
+      <MyList :todos="todos"/>
+      <MyFooter/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MyHeader from "./components/MyHeader";
+  import MyList from "./components/MyList";
+  import MyFooter from "./components/MyFooter";
+
+  export default {
+    name: "App",
+    components: { MyHeader, MyList, MyFooter },
+    data() {
+            return {
+                todos:[
+                    {id: '001', title: "吃饭", done: true},
+                    {id: '002', title: "抽烟", done: false},
+                    {id: '003', title: "喝酒", done: false},
+                    {id: '004', title: "洗脚", done: true},
+                ]
+            }
+        },
+    methods:{
+      addTodo(todoObj){
+        console.log("App组件接受到了数据：", todoObj);
+        this.todos.unshift(todoObj)
+      }
+    }
+  };
+</script>
+
+<style>
+  /* base */
+  body {
+    background: #fff;
+  }
+  .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+  }
+  .btn-danger {
+    color:#fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+  }
+  .btn-danger:hover {
+    color:#fff;
+    background-color: #bd362f;
+  }
+  .btn:focus {
+    outline: none;
+  }
+  .todu-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todu-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+
+</style>
---
+++ 20.脚手架创建项目/vue_test/09.src_TodoList案例3_添加数据/components/MyHeader.vue
@@ -0,0 +1,54 @@
+<!-- 顶部输入框组件 -->
+<template>
+  <div class="todo-header">
+    <input
+      type="text"
+      placeholder="请输入你的任务，按回车确认"
+      v-model="title"
+      @keyup.enter="add"
+    />
+  </div>
+</template>
+
+<script>
+import { nanoid } from 'nanoid' // 生成uuid
+export default {
+  name: "MyHeader",
+  props: ["addTodo"],
+  data() {
+    return {
+        title: '',
+    }
+  },
+  methods: {
+    add() {
+        // 校验空数据
+        if(!this.title.trim()) return alert("输入不能为空")
+        // 将用户输入得数据包装成对象
+        const todoObj = { id: nanoid(), title: this.title, done: false };
+        // 回调数据给App组件，添加一个数据
+        this.addTodo(todoObj)
+        // 清空输入框
+        this.title = ''
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* header */
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
+    0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
