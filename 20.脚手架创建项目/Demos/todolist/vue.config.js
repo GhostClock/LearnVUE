51a8dfa5e6d32cc47c8e045ee8ae580fa6b5e8ec
GhostClock <zhangyuanlaifen@gmail.com>
2023-02-11 10:01:40 +0800

    抽取项目到Demos目录：初始化TodoList和实现静态组件

---
+++ 20.脚手架创建项目/Demos/todolist/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pages: {
+    index: {
+      // 自定义入口文件
+      entry: 'src/main.js'
+    },
+  },
+  lintOnSave: false, // 忽略语法规则错误
+})
