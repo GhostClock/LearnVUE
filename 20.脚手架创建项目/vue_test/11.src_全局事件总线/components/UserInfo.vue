4131ee36f31e3d3dad98be2b3fab9e673ed46865
GhostClock <zhangyuanlaifen@gmail.com>
2023-02-19 21:13:23 +0800

    归档：全局事件总线

---
+++ 20.脚手架创建项目/vue_test/11.src_全局事件总线/components/UserInfo.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="userInfo">
+    <h2>用户姓名：{{ name }}</h2>
+    <h2>用户性别：{{ sex }}</h2>
+    <button @click="sendUserInfoName">把用户名传递给WebSite组件</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "UserInfo",
+        data() {
+            return {
+                name: "张三",
+                sex: "男生",
+            }
+        },
+        methods: {
+          sendUserInfoName() {
+            this.$bus.$emit("hello", this.name)
+          }
+        },
+    }
+</script>
+
+<style scoped>
+  .userInfo {
+    background-color: blueviolet;
+    padding: 5px;
+    margin-top: 30px;
+  }
+</style>
---
+++ 20.脚手架创建项目/vue_test/11.src_全局事件总线/components/WebSite.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div class="school">
+    <h2>网站名称：{{ name }}</h2>
+    <h2>网站地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "WebSite",
+        data() {
+            return {
+                name: "百度",
+                address: "北京",
+            }
+        },
+        mounted() {
+          this.$bus.$on("hello", (data) => {
+            console.log("我是WebSite组件,收到了数据", data);
+          })
+        },
+        beforeDestroy() {
+          this.$bus.$off("hello") // 解绑事件总线
+        },
+    }
+</script>
+
+<style scoped>
+  .school {
+    background-color: aqua;
+    padding: 5px;
+    margin-top: 30px;
+  }
+</style>
