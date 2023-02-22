ae79b03e6ef85b7bfb475beb04c6cce50734492e
GhostClock <zhangyuanlaifen@gmail.com>
2023-02-22 21:09:19 +0800

    归档：过度与动画

---
+++ 20.脚手架创建项目/vue_test/13.src_过度与动画/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <TestAction />
+    <TestAction2 />
+    <TestAction3 />
+  </div>
+</template>
+
+<script>
+  import TestAction from "./components/TestAction.vue"
+  import TestAction2 from "./components/TestAction2.vue"
+  import TestAction3 from "./components/TestAction3.vue"
+  export default {
+    name: "App",
+    components: { TestAction, TestAction2, TestAction3 },
+    data() {
+      return {
+        msg: "你好啊!!!",
+      }
+    },
+  }
+</script>
