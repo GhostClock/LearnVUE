9584fe62b1c0e7ea6b89d620772746732b165e87
GhostClock <zhangyuanlaifen@gmail.com>
2023-02-23 23:08:58 +0800

    归档：配置代理服务器

---
+++ 20.脚手架创建项目/vue_test/14.src_配置代理服务器/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div >
+    <button @click="getStudents">获取学生信息</button>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: "App",
+    methods: {
+      getStudents(){
+        // 配置了代理服务器，向代理服务请求数据
+        // 请求流程：axios -> proxyServer -> remoteServer
+        axios.get("http://localhost:8080/ghostclock/get").then(
+          response => {
+            console.log("请求成功了", response.data);
+          },
+          error => {
+            console.log("请求失败", error.message);
+          }
+        )
+      }
+    },
+  }
+</script>
