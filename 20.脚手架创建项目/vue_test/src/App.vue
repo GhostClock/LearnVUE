0d6d8657108c876b90c7a1a834b7aca95fdf4d6f
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-04 11:00:36 +0800

    插槽:1.默认插槽

--- 20.脚手架创建项目/vue_test/src/App.vue
+++ 20.脚手架创建项目/vue_test/src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <div >
-    <button @click="getStudents">获取学生信息</button>
+  <div class="container">
+    <CategoryList title="美食">
+      <img src="https://img2.baidu.com/it/u=2192935436,1889428079&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800" alt="火锅">
+    </CategoryList>
+
+    <CategoryList title="游戏" :listData="games" >
+      <ul>
+        <li v-for="(game, index) in games" :key="index"> {{ game }} </li>
+      </ul>
+    </CategoryList>
+
+    <CategoryList title="电影">
+      <video controls autoplay>
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+      </video>
+    </CategoryList>
+
   </div>
 </template>
 
 <script>
-  // import axios from 'axios'
+  import  CategoryList  from "./components/CategoryList.vue";
   export default {
     name: "App",
-    methods: {
-      getStudents(){
-        // 配置了代理服务器，向代理服务请求数据
-        // 请求流程：axios -> proxyServer -> remoteServer
-        this.$http.get("http://localhost:8080/ghostclock/get").then(
-          response => {
-            console.log("请求成功了", response.data);
-          },
-          error => {
-            console.log("请求失败", error.message);
-          }
-        )
+    components: { CategoryList },
+    data() {
+      return {
+        foods: ["火锅", "牛羊", "小龙虾", "牛排"],
+        games: ["红色警戒", "穿越火线", "QQ劲舞团", "超级玛丽"],
+        films: ["《教父》", "《流浪地球》", "《三体-死神永生》"],
       }
     },
   }
 </script>
+
+<style scoped>
+  .container {
+    /* 横向布局 */
+    display: flex;
+    justify-content: space-around;
+  }
+  img {
+    width: 100%;
+  }
+  video{
+    width: 100%;
+  }
+</style>
--- 20.脚手架创建项目/vue_test/src/main.js
+++ 20.脚手架创建项目/vue_test/src/main.js
@@ -3,12 +3,9 @@
 import Vue from 'vue'
 // 引入App
 import App from './App.vue'
-// 引入vue-resource插件
-import vueResource from 'vue-resource'
+
 // 关闭Vue生产提示
 Vue.config.productionTip = false
-// 使用引入的插件
-Vue.use(vueResource)
 
 // 创建vm
 new Vue({
