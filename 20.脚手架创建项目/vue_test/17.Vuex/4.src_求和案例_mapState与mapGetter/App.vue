2bc359612816eca3c6445bb18f39eaa3f1c85c11
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-05 21:37:43 +0800

    归档：Vuex：mapState与mapGetters的用法

---
+++ 20.脚手架创建项目/vue_test/17.Vuex/4.src_求和案例_mapState与mapGetter/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Count/>
+  </div>
+</template>
+
+<script>
+  import Count from "./components/Count.vue";
+  export default {
+    name: "App",
+    components: { Count },
+  }
+</script>
+
+<style scoped>
+  .container, .foot {
+    /* 横向布局 */
+    display: flex;
+    justify-content: space-around;
+  }
+  img {
+    width: 100%;
+  }
+  video{
+    width: 100%;
+  }
+  h4{
+    text-align: center;
+  }
+</style>
---
+++ 20.脚手架创建项目/vue_test/17.Vuex/4.src_求和案例_mapState与mapGetter/components/Count.vue
@@ -0,0 +1,95 @@
+<template>
+  <div >
+    <h1>当前求和为：{{ sum }} </h1>
+    <h3>放大10倍后为：{{ bigSum }} </h3>
+    <h3>我在{{ school }}，学习{{ subject }} </h3>
+    <select v-model.number="number">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapGetters } from "vuex";
+  export default {
+    name: "Count",
+    data() {
+      return {
+        number: 1, //用户选择的数字
+      }
+    },
+    computed: {
+      // 靠程序员自己生成的计算属性
+      /*
+      sum(){
+        return this.$store.state.sum
+      },
+      school() {
+        return this.$store.state.school
+      },
+      subject() {
+        return this.$store.state.subject
+      },
+      */
+      // 借助mapState生成计算属性，从state中读取数据 --（对象写法）
+      // ...mapState({he: 'sum', xuexiao: 'school', xueke: 'subject'}),
+
+      // 借助mapState生成计算属性，从state中读取数据 --（数组写法）
+      ...mapState(['sum', 'school', 'subject']),
+
+      /*
+      bigSum() {
+        return this.$store.getters.bigSum
+      }
+      */
+      // 借助mapGetters生成计算属性，从getters中读取数据 --（对象写法）
+      // ...mapGetters({bigSum: 'bigSum'}),
+      
+      // 借助mapGetters生成计算属性，从getters中读取数据 --（数组写法）
+      ...mapGetters(['bigSum']),
+     
+    },
+    methods: {
+      // +
+      increment() {
+        // Vuex：第一步
+        // this.$store.dispatch('add', this.number)
+        // 无重要处理逻辑，可以直接向mutations发送数据
+        this.$store.commit('ADD', this.number)
+      },
+
+      // -
+      decrement() {
+        // this.$store.dispatch('sub', this.number)
+        this.$store.commit('SUB', this.number)
+      },
+
+      // 当前求和为奇数再加
+      incrementOdd() {
+        this.$store.dispatch('addOdd', this.number)
+      },
+
+      // 等一等再加
+      incrementWait() {
+        this.$store.dispatch('addWait', this.number)    
+      },
+    },
+    mounted() {
+      console.log("Count组件", this);
+    }
+  }
+</script>
+
+<style>
+  button {
+    margin-left: 5px;
+  }
+  
+</style>
