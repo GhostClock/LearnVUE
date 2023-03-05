a7bd8244772113daf22bcc083debe1da10870bcb
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-05 10:52:52 +0800

    归档：具名插槽

---
+++ 20.脚手架创建项目/vue_test/16.插槽/2.src_具名插槽/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <CategoryList title="美食">
+      <img slot="center" src="https://img2.baidu.com/it/u=2192935436,1889428079&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800" alt="火锅">
+      <a class="foot" slot="footer" href="https://www.baidu.com">更多美食</a>
+    </CategoryList>
+
+    <CategoryList title="游戏" :listData="games" >
+      <ul slot="center">
+        <li v-for="(game, index) in games" :key="index"> {{ game }} </li>
+      </ul>
+      <div class="foot" slot="footer">
+        <a href="https://www.baidu.com">单击游戏</a>
+        <a href="https://www.baidu.com">网络游戏</a>
+      </div>
+    </CategoryList>
+
+    <CategoryList title="电影">
+      <!-- autoplay去掉自动播放 -->
+      <video slot="center" controls> 
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+      </video>
+      <!-- 使用template时，除了slot="footer", 还可以v-slot:footer -->
+      <template v-slot:footer>
+        <div class="foot">
+          <a href="https://www.baidu.com">经典</a>
+          <a href="https://www.baidu.com">热门</a>
+          <a href="https://www.baidu.com">推荐</a>
+        </div>
+        <h4>欢迎前来观影</h4>
+      </template>
+    </CategoryList>
+
+  </div>
+</template>
+
+<script>
+  import  CategoryList  from "./components/CategoryList.vue";
+  export default {
+    name: "App",
+    components: { CategoryList },
+    data() {
+      return {
+        foods: ["火锅", "牛羊", "小龙虾", "牛排"],
+        games: ["红色警戒", "穿越火线", "QQ劲舞团", "超级玛丽"],
+        films: ["《教父》", "《流浪地球》", "《三体-死神永生》"],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .container, .foot {
+    /* 横向布局 */
+    display: flex;
+    justify-content: space-around;
+  }
+  img {
+    width: 100%;
+  }
+  video{
+    width: 100%;
+  }
+  h4{
+    text-align: center;
+  }
+</style>
---
+++ 20.脚手架创建项目/vue_test/16.插槽/2.src_具名插槽/components/CategoryList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <!-- 定义一个插槽(挖个坑，等待组件的使用者来填充) -->
+    <slot name="center">这是默认值，没有传递时会显示出来1</slot>
+    <slot name="footer">这是默认值，没有传递时会显示出来2</slot>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "CategoryList",
+      props: [ "title", "listData"]
+    }
+</script>
+
+<style scoped>
+  .category {
+    background-color: skyblue;
+    width: 200px;
+    height: 300px;
+  }
+  h3 {
+    text-align: center;
+    background-color: rgb(249, 168, 19);
+  }
+  
+</style>
