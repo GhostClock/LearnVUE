0f815e5bae07edebbf893b3f0968d40f9cc9ad10
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-05 23:00:09 +0800

    归档：Vuex多组件共享数据

---
+++ 20.脚手架创建项目/vue_test/17.Vuex/6.src_求和案例_多组件共享数据/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Count/>
+    <hr/>
+    <Person/>
+  </div>
+</template>
+
+<script>
+  import Count from "./components/Count.vue";
+  import Person from './components/Person.vue'
+  export default {
+    name: "App",
+    components: { Count, Person},
+  }
+</script>
+
+<style scoped>
+  .container, .foot {
+    /* 横向布局 */
+    display: flex;
+    justify-content: space-around;
+  }
+  img {
+    width: 100%;
+  }
+  video{
+    width: 100%;
+  }
+  h4{
+    text-align: center;
+  }
+</style>
---
+++ 20.脚手架创建项目/vue_test/17.Vuex/6.src_求和案例_多组件共享数据/components/Person.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>人员列表</h1>
+    <h3 style="color: blue">Count组件的求和为：{{ sum }}</h3>
+    <input v-model="name" type="text" placeholder="请输入名字"/>
+    <button @click="add">添加</button>
+    <ul>
+        <li v-for="p in personList" :key="p.id">{{ p.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    import { nanoid } from "nanoid";
+    export default {
+        name: 'Person',
+        data() {
+            return {
+                name: ''
+            }
+        },
+        methods: {
+            add() {
+                if (!this.name) return alert('输入不能为空')
+                const personObj = {id: nanoid(), name: this.name}
+                console.log(personObj);
+                this.$store.commit('ADD_PERSON', personObj)
+                this.name = ''
+            }
+        },
+        computed:{
+            personList() {
+                return this.$store.state.personList
+            },
+            sum() {
+                return this.$store.state.sum
+            },
+        },
+    }
+</script>
+
+<style>
+
+</style>
