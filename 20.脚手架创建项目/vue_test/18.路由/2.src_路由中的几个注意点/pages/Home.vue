a54767d4cd59524303a106128ba62b1abc5c8801
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-06 22:03:02 +0800

    归档：路由中的几个注意点

---
+++ 20.脚手架创建项目/vue_test/18.路由/2.src_路由中的几个注意点/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        mounted() {
+          console.log("Home组件挂载完毕", this);
+        },
+        beforeDestroy() {
+          console.log("Home组件即将被销毁");
+        },
+    }
+</script>
---
+++ 20.脚手架创建项目/vue_test/18.路由/2.src_路由中的几个注意点/router/index.js
@@ -0,0 +1,22 @@
+// 该文件专门用于创建整个应用的路由器
+
+// 引入vuerouter
+import VueRouter from "vue-router"
+
+// 引入组件
+import About from '../pages/About.vue'
+import Home from '../pages/Home.vue'
+
+// 创建并暴露一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About
+        },
+        {
+            path: '/home',
+            component: Home
+        },
+    ]
+})
