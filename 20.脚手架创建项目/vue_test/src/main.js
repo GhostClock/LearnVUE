e1250e7c70c3dc9716502ba006a513e0e9dc64c4
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-06 21:30:52 +0800

    路由：路由的基本使用

--- 20.脚手架创建项目/vue_test/src/main.js
+++ 20.脚手架创建项目/vue_test/src/main.js
@@ -3,14 +3,18 @@
 import Vue from 'vue'
 // 引入App
 import App from './App.vue'
-//引入store
-import store from './store/index'
+// 引入vue-router
+import VueRouter from 'vue-router'
+// 引入路由器
+import router from './router'
 // 关闭Vue生产提示
 Vue.config.productionTip = false
+// 使用VueRouter
+Vue.use(VueRouter)
 
 // 创建vm
 new Vue({
      el: "#app",
-     store,
-     render: h => h(App)
+     render: h => h(App),
+     router,
 })
