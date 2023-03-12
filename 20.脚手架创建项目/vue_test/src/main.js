7d35d63aff56c8163b73956486399bebd3341748
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-12 22:21:01 +0800

    ElementUI：按需引入

--- 20.脚手架创建项目/vue_test/src/main.js
+++ 20.脚手架创建项目/vue_test/src/main.js
@@ -5,13 +5,21 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // 全部引入ElementUI
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 // 全部引入ElementUI全部样式
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// 按需引入
+import { Button, Row, DatePicker } from 'element-ui';
+
 // 关闭Vue生产提示
 Vue.config.productionTip = false
 // 使用ElementUI
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+
+Vue.component(Button.name, Button)
+Vue.component(Row.name, Row)
+Vue.component(DatePicker.name, DatePicker)
 
 // 创建vm
 new Vue({
