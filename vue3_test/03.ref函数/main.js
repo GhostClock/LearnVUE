2c94e3deb4b51ac761e2b0d5d8de821a22e0b3b9
GhostClock <zhangyuanlaifen@gmail.com>
2023-04-28 22:25:01 +0800

    ref函数

---
+++ vue3_test/03.ref函数/main.js
@@ -0,0 +1,9 @@
+// 引入的不再是Vue的构造函数了
+// 引入的是一个createApp的工厂函数
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 成绩应用实例对象 app 类似于Vue2中的vm，但是比vm轻
+const app =  createApp(App)
+// 挂载
+app.mount('#app')
