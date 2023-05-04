ca374a76418547485fcbbfb93bfa5927d19c9967
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-05 00:03:48 +0800

    readonly与shallowReadonly函数

---
+++ vue3_test/15.readonly与shallowReadonly函数/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="isShow = !isShow">切换隐藏/显示</button>
+  <Demo v-if="isShow" />
+</template>
+
+<script>
+import { ref } from "vue";
+import Demo from "./components/Demo.vue";
+export default {
+  name: "App",
+  components: {
+    Demo,
+  },
+  setup() {
+    let isShow = ref(true);
+
+    return {
+      isShow,
+    };
+  },
+};
+</script>
