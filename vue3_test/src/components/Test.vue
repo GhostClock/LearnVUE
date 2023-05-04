2524155cfa65b3f3eed2eb076a8b6e972827ed73
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-04 21:41:40 +0800

    自定义hook函数

---
+++ vue3_test/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>我是Test组件</h1>
+    <h2>x：{{ point.x }}  y：{{ point.y }}</h2>
+</template>
+<script>
+import usePoint from '../hooks/usePoint.js'
+export default {
+  name: "Test",
+  setup() {
+    const point = usePoint()
+    
+    return {
+        point
+    }
+  }
+};
+</script>
