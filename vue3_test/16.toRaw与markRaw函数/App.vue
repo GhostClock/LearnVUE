938f46eb52959e93d2afad0a5bf8f5d28d036696
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-05 23:49:54 +0800

    toRaw与markRaw函数

---
+++ vue3_test/16.toRaw与markRaw函数/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="isShow = !isShow">切换隐藏/显示</button>
+  <Demo v-if="isShow" />
+</template>
+
+<script>
+import { ref } from "vue";
+import Demo from "./components/Demo.vue";
+export default {
+  name: "App",
+  components: {
+    Demo,
+  },
+  setup() {
+    let isShow = ref(true);
+
+    return {
+      isShow,
+    };
+  },
+};
+160
+</script>
