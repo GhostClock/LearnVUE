9ee6f8309fd2b17e3d2dd8ecf6dbc0097a14107d
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-07 16:17:43 +0800

    provide和inject函数

---
+++ vue3_test/18.provide和inject函数/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="child">
+    <h3>我是Child组件(子)</h3>
+    <Son />
+  </div>
+</template>
+
+<script>
+// import { inject } from "vue";
+import Son from "./Son";
+export default {
+  name: "Child",
+  components: {
+    Son,
+  },
+  setup() {
+    // let car = inject('Car')
+    // return { car }
+  }
+};
+</script>
+
+<style>
+.child {
+  background: skyblue;
+  padding: 10px;
+}
+</style>
