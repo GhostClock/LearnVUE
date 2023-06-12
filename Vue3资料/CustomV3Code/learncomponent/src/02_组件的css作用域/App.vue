ed0456c6895d9d2849546cfdc1ba81e03cbfc0c5
GhostClock <zhangyuanlaifen@gmail.com>
2023-06-12 19:44:45 +0800

    组件的css作用域问题

---
+++ Vue3资料/CustomV3Code/learncomponent/src/02_组件的css作用域/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class="title">App</h2>
+  <HelloWord />
+</template>
+
+<script>
+  import HelloWord from './HelloWorld.vue'
+  export default {
+    components: {
+      HelloWord
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  h2 {
+    color: red;
+  }
+  .title {
+    color: blue;
+  }
+</style>
