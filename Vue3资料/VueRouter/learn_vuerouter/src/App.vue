f40f9270cc55caf1207e46436c7696954dddd5b4
GhostClock <zhangyuanlaifen@gmail.com>
2023-07-06 22:05:26 +0800

    router-link中的作用域插槽

--- Vue3资料/VueRouter/learn_vuerouter/src/App.vue
+++ Vue3资料/VueRouter/learn_vuerouter/src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <router-link to="/home">首页</router-link>
+  <!-- 
+    props: href 跳转的链接
+    props: route对象
+    props: navigate导航函数
+    props: isActive 当前是否处于活跃状态
+    props: isExactActive 当前是否处于精确的活跃状态
+   -->
+  <router-link to="/home" v-slot="props" custom>
+    <NavBar title="首页" />
+    <button @click="props.navigate">测试文本</button>
+    <span>{{ props.href }}</span>
+    <!-- <span>{{ props.route }}</span> -->
+    <!-- <span>{{ props.navigate }}</span> -->
+    <span :class="{'active': props.isActive}">{{ props.isActive }}</span>
+    <span :class="{'active': props.isExactActive}">{{ props.isExactActive }}</span>
+
+  </router-link>
+
   <router-link to="/about">关于</router-link>
   <router-link :to="'/user/' + name + '/id/' + 11" >用户</router-link>
 
@@ -10,6 +27,9 @@
 
 <script setup>
   import { useRouter } from 'vue-router'
+
+  import NavBar from './components/NavBar.vue';
+
   const name = 'cg'
   
   const router = useRouter()
---
+++ Vue3资料/VueRouter/learn_vuerouter/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2> {{ title }}</h2>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from "vue";
+
+  defineProps({
+    title: {
+      type: String,
+      default: '默认标题'
+    }
+  })
+
+  
+</script>
+
+<style lang="css" scoped>
+
+</style>
