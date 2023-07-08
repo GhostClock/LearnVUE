47a3c6ddda863d42ea1263c66b46a19ce145968f
GhostClock <zhangyuanlaifen@gmail.com>
2023-07-08 19:55:59 +0800

    mapMutations的使用

---
+++ Vue3资料/Vuex/learn_vuex/src/pages/06.mutations的辅助函数.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>当前计数： {{ counter }}</h2>
+    <hr>
+      <button @click="increment">+1</button>
+      <button @click="decrement">-1</button>
+      <button @click="incrementN">+10</button>
+    <hr> 
+  </div>
+</template>
+
+<script setup>
+  import { mapMutations } from "vuex";
+  import { useState } from "../hooks";
+  // import { INCREMENT_N } from "../store/mutation-typs";
+
+  const { counter } = useState(['counter'])
+  
+  const { increment } = mapMutations(['increment'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
